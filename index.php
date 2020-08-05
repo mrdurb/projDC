922b451b9060314d715a21e29ad995726a9ab1e4
ilxappp <mr.goldanroch@gmail.com>
2020-08-05 12:29:06 +0700

    scripts

--- index.php
+++ index.php
@@ -8,20 +8,14 @@
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <ul class="nav nav-tabs">
-        <li class="nav-item" id="active2">
-            <a class="nav-link" href="" id="link" onclick="return false;">Задачи</a>
+    <ul class="nav nav-tabs" id="tabs">
+        <li class="nav-item tab">
+            <a class="nav-link" href="#">Задачи</a>
         </li>
-        <li class="nav-item" id="active">
-            <a class="nav-link" href="" id="link" onclick="return false;">Исполнители</a>
+        <li class="nav-item tab">
+            <a class="nav-link" href="#">Исполнители</a>
         </li>
     </ul>
-    <script>
-            $("#tabs").on("click", ".tab", function(){
-                $("#tabs .tab").removeClass("active"); //удаляем класс во всех вкладках
-                $(this).addClass("active"); //добавляем класс текущей (нажатой)
-            });
-    </script>
     
 
 
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// $(document).ready(function(){
-// 	$('#link').click(function () {
-// 		$('#link').toggleClass('click');
-// 		});
-// });
+//отмена переход по ссылке
+$("#tabs .tab").on("click", "a", function(e){
+    e.preventDefault(); 
+});
+// активация кнопки
+$(".nav").find("a").click(function(){   
+    $(".nav .active").removeClass("active");
+    $(this).addClass("active");
+});
