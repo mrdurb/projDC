e65a5c9588fb7653288bf20971a08bcef60fb004
ilxappp <mr.goldanroch@gmail.com>
2020-08-05 13:17:20 +0700

    add tabs

--- index.php
+++ index.php
@@ -4,24 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>projDC</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-    <ul class="nav nav-tabs" id="tabs">
-        <li class="nav-item tab">
-            <a class="nav-link" href="#">Задачи</a>
-        </li>
-        <li class="nav-item tab">
-            <a class="nav-link" href="#">Исполнители</a>
-        </li>
-    </ul>
-    
+    <div class="container mt-5">
+        <ul class="nav nav-tabs ">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#description">Задачи</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#characteristics">Исполнители</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane  show active" id="description">
+                Описание товара...
+            </div>
+            <div class="tab-pane " id="characteristics">
+                Характеристики товара...
+            </div>
+        </div>
+    </div>
 
-
-
-
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous" ></script>
     <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++
@@ -1,9 +0,0 @@
-//отмена переход по ссылке
-$("#tabs .tab").on("click", "a", function(e){
-    e.preventDefault(); 
-});
-// активация кнопки
-$(".nav").find("a").click(function(){   
-    $(".nav .active").removeClass("active");
-    $(this).addClass("active");
-});
